b99cd47b699532197526f84ab22234de28d9f336
Toni Mertanen <t.mertanen@hotmail.com>
2024-01-07 00:31:01 +0200

    Navbar prototype added

--- index.js
+++ index.js
@@ -9,3 +9,5 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {res.render(__dirname + "/views/index.ejs")});
+
+app.listen(port, () => console.log(`App listening on port ${port}.`));
