6bf96fb9fa3b5ea0aa39832909cb7a4c767fe25c
Toni Mertanen <t.mertanen@hotmail.com>
2024-01-09 18:59:26 +0200

    Added navigation to home link

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {res.render(__dirname + "/views/index.ejs")});
+app.get("/home", (req, res) => {res.render(__dirname + "/views/index.ejs")});
 app.get("/newpost", (req, res) => {res.render(__dirname + "/views/newpost.ejs")});
 
 app.listen(port, () => console.log(`App listening on port ${port}.`));
