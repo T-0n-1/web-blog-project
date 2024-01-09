2709037f9cc0a60bfdd5de19e313ed1d41d225cc
Toni Mertanen <t.mertanen@hotmail.com>
2024-01-09 22:04:53 +0200

    Browser page started

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ app.use(express.static("public"));
 
 app.get("/", (req, res) => {res.render(__dirname + "/views/index.ejs")});
 app.get("/home", (req, res) => {res.render(__dirname + "/views/index.ejs")});
+app.get("/browse", (req, res) => {res.render(__dirname + "/views/browse.ejs")});
 app.get("/newpost", (req, res) => {res.render(__dirname + "/views/newpost.ejs")});
 
 app.listen(port, () => console.log(`App listening on port ${port}.`));
