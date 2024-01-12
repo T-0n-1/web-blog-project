1c616d765652a675eb44bd8a93113af4346ce5d0
Toni Mertanen <t.mertanen@hotmail.com>
2024-01-12 19:51:12 +0200

    New post submitting handler started

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
 import express from "express";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
+import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {res.render(__dirname + "/views/index.ejs")});
 app.get("/home", (req, res) => {res.render(__dirname + "/views/index.ejs")});
 app.get("/browse", (req, res) => {res.render(__dirname + "/views/browse.ejs")});
 app.get("/newpost", (req, res) => {res.render(__dirname + "/views/newpost.ejs")});
+
 app.post("/search", (req, res) => {
     res.render(__dirname + "/views/results.ejs")});
+
 app.post("/submit", (req, res) => {
-    res.render(__dirname + "/views/results.ejs")});
+    console.log("Submitted data:", req.body);
+    res.render(__dirname + "/views/newpost.ejs");
+});
 
 app.listen(port, () => console.log(`App listening on port ${port}.`));
